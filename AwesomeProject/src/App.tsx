3b81e1b09a39ed35133852088bec6e303f2f61df
Data Analytics3 Cell 정다예 <daye.jeong@sk.com>
2020-04-06 17:58:40 +0900

    - add counter

---
+++ AwesomeProject/src/App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Styled from 'styled-components/native';
+import Counter from './Screens/Counter';
+
+
+const Container = Styled.View`
+    flex:1;
+    background-color: #EEE;
+`;
+
+
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return (
+      <Container>
+          <Counter title="This is a Counter App" initValue={5} />
+      </Container>
+    
+  );
+};
+
+export default App;
