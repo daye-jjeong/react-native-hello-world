3f2ce4550e9f5a68538992e8deffb7a995f6c4f7
Data Analytics3 Cell 정다예 <daye.jeong@sk.com>
2020-04-06 18:03:56 +0900

    - src 절대 경로 적용

--- AwesomeProject/src/App.tsx
+++ AwesomeProject/src/App.tsx
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import Styled from 'styled-components/native';
-import Counter from './Screens/Counter';
+import Counter from '~/Screens/Counter';
 
 
 const Container = Styled.View`
